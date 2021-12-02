9a2105cf9ab6fca991c5ab27e5ba16daa014fce9
beke83 <55108249+beke83@users.noreply.github.com>
2021-12-02 22:03:23 +0100

    Additional Files

--- include/Functions.php
+++ include/Functions.php
@@ -2,37 +2,60 @@
 <?php require_once("../include/session.php"); ?> 
 <?php
 
-    function Redirect_to($New_Location){
-        header("Location:". $New_Location);
-        exit;
-    }
+function Redirect_to($New_Location)
+{
+    header("Location:" . $New_Location);
+    exit;
+}
 
-    function Login_Attempt($Username,$Password){
-        global $Connection;
-        $Query = "SELECT * FROM admins_tbl 
+function Login_Attempt($Username, $Password)
+{
+    global $Connection;
+    $Query = "SELECT * FROM admins_tbl 
         WHERE username='$Username' AND password = '$Password'";
-        $Execute =  mysqli_query($Connection, $Query);
-        if($admin = mysqli_fetch_assoc($Execute)){
-            return $admin;
-        }
-        else{
-            return null;
-        }
-
+    $Execute =  mysqli_query($Connection, $Query);
+    if ($admin = mysqli_fetch_assoc($Execute)) {
+        return $admin;
+    } else {
+        return null;
     }
+}
 
-    function Login(){
-        if(isset($_SESSION["id"])){
-            return true;
-        }
+function Login()
+{
+    if (isset($_SESSION["id"])) {
+        return true;
     }
+}
 
-    function confirm_login(){
-        if(!Login()){
-            $_SESSION["ErrorMessage"] = "Login Required!";
-            Redirect_to("adminLogin.php");
-        }
+function confirm_login()
+{
+    if (!Login()) {
+        $_SESSION["ErrorMessage"] = "Login Required!";
+        Redirect_to("adminLogin.php");
     }
+}
+
+function confirm_patient_login()
+{
+    if (!Login()) {
+        $_SESSION["ErrorMessage"] = "Login Required!";
+        Redirect_to("patientLogin.php");
+    }
+}
+
+function confirm_doctor_login()
+{
+    if (!Login()) {
+        $_SESSION["ErrorMessage"] = "Login Required!";
+        Redirect_to("doctorLogin.php");
+    }
+}
+
+function randomString(){
+    $permitted_chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    echo substr(str_shuffle($permitted_chars), 0, 8);
+}
 
 
 ?>
--- include/db_connect.php
+++ include/db_connect.php
@@ -1,6 +1,10 @@
+<!-- This is the code to connect to the database-->
 <?php
 
-$db_connect =mysqli_connect("localhost","root","yahoo@");
-mysqli_select_db($db_connect,"daiohms") or die(mysqli_error($db_connect));
+// localhost- server name
+// root - username
+// yahoo@ - password
+$db_connect = mysqli_connect("localhost", "root", "yahoo@");
+mysqli_select_db($db_connect, "daiohms") or die(mysqli_error($db_connect));
 
 ?>
